bf74abd70d1fe5a4fd2ce431b9a19c7b87a7a026
wonkenstein <marky.wong@gmail.com>
2017-03-27 12:30:17 +0100

    End tutorial 2

---
+++ workspace/src/index.js
@@ -0,0 +1,13 @@
+// @flow
+/* eslint-disable no-console */
+
+// const Dog = require('./dog');
+import Dog from './dog'; // // ES6
+
+const str = 'ES6';
+console.log(`Hello ${str}`);
+
+
+const toby = new Dog('Toby');
+
+console.log(toby.bark());
